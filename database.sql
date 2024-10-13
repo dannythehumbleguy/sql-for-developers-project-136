9e726081e966c838f43ca0830d55347dcc28c087
Danil Zhiltsov <dannygam47@gmail.com>
2024-10-13 19:38:23 +0300

    fix names

--- database.sql
+++ database.sql
@@ -1,68 +1,75 @@
 ﻿CREATE TABLE courses
 (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    title VARCHAR(256) NOT NULL,
+    name VARCHAR(256) NOT NULL,
     description VARCHAR(2048) NOT NULL,
     created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP NULL
+    updated_at TIMESTAMP NULL,
+    deleted_at TIMESTAMP NULL
 );
 
 CREATE TABLE lessons
 (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    title VARCHAR(256) NOT NULL,
-    payload VARCHAR(8192) NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    content VARCHAR(8192) NOT NULL,
     video_url VARCHAR(256) NOT NULL,
     position INT NOT NULL,
     course_id BIGINT REFERENCES courses (id) NOT NULL,
     created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP NULL
+    updated_at TIMESTAMP NULL,
+    deleted_at TIMESTAMP NULL
 );
 
 CREATE TABLE quizzes
 (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    title VARCHAR(256) NOT NULL,
-    text VARCHAR(2048) NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    content VARCHAR(2048) NOT NULL,
     lesson_id BIGINT REFERENCES lessons (id) NOT NULL,
     created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP NULL
+    updated_at TIMESTAMP NULL,
+    deleted_at TIMESTAMP NULL
 );
 
 CREATE TABLE exercises
 (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    title VARCHAR(256) NOT NULL,
+    name VARCHAR(256) NOT NULL,
     lesson_id BIGINT REFERENCES lessons (id) NOT NULL,
     url VARCHAR(256) NOT NULL,
     created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP NULL
+    updated_at TIMESTAMP NULL,
+    deleted_at TIMESTAMP NULL
 );
 
 CREATE TABLE modules
 (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    title VARCHAR(256) NOT NULL,
+    name VARCHAR(256) NOT NULL,
     description VARCHAR(2048) NOT NULL,
     created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP NULL
+    updated_at TIMESTAMP NULL,
+    deleted_at TIMESTAMP NULL
 );
 
-CREATE TABLE module_courses
+CREATE TABLE course_modules
 (
     course_id BIGINT REFERENCES courses (id) NOT NULL,
     module_id BIGINT REFERENCES modules (id) NOT NULL,
-    PRIMARY KEY (module_id, course_id)
+    PRIMARY KEY (module_id, course_id),
+    deleted_at TIMESTAMP NULL
 );
 
 CREATE TABLE programs
 (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    title VARCHAR(256) NOT NULL,
+    name VARCHAR(256) NOT NULL,
     price DECIMAL(12, 2) NOT NULL,
-    type VARCHAR(16) NOT NULL ,
+    program_type VARCHAR(16) NOT NULL ,
     created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP NULL
+    updated_at TIMESTAMP NULL,
+    deleted_at TIMESTAMP NULL
 );
 
 CREATE TABLE program_modules
@@ -75,21 +82,23 @@ CREATE TABLE program_modules
 CREATE TABLE teaching_groups
 (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    slack_url VARCHAR(256) NOT NULL,
+    slug VARCHAR(256) NOT NULL,
     created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP NULL
+    updated_at TIMESTAMP NULL,
+    deleted_at TIMESTAMP NULL
 );
 
 CREATE TABLE users
 (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     email VARCHAR(128) UNIQUE NOT NULL,
-    password VARCHAR(512) NOT NULL,
+    password_hash VARCHAR(512) NOT NULL,
     name VARCHAR(64) NOT NULL,
     role VARCHAR(32) NOT NULL,
     teaching_group_id BIGINT REFERENCES teaching_groups (id) NOT NULL,
     created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP NULL
+    updated_at TIMESTAMP NULL,
+    deleted_at TIMESTAMP NULL
 );
 
 CREATE TYPE enrollment_status AS ENUM ('active', 'pending', 'cancelled', 'completed');
@@ -100,7 +109,8 @@ CREATE TABLE enrollments
     user_id BIGINT REFERENCES users (id) NOT NULL,
     status enrollment_status NOT NULL,
     created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP NULL
+    updated_at TIMESTAMP NULL,
+    deleted_at TIMESTAMP NULL
 );
 
 CREATE TYPE payment_status AS ENUM ('pending', 'paid', 'failed', 'refunded');
@@ -108,11 +118,12 @@ CREATE TABLE payments
 (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     enrollment_id BIGINT REFERENCES enrollments (id) NOT NULL,
-    amount DECIMAL(12, 2) NOT NULL,
+    amount DECIMAL(12, 2) NULL,
     status payment_status NOT NULL,
-    payed_at TIMESTAMP NOT NULL,
+    paid_at TIMESTAMP NULL,
     created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP NULL
+    updated_at TIMESTAMP NULL,
+    deleted_at TIMESTAMP NULL
 );
 
 CREATE TYPE program_completion_status AS ENUM ('active', 'completed', 'pending', 'cancelled');
@@ -123,9 +134,10 @@ CREATE TABLE program_completions
     user_id BIGINT REFERENCES users (id) NOT NULL,
     status program_completion_status NOT NULL,
     started_at TIMESTAMP NULL,
-    ended_at TIMESTAMP NULL,
+    completed_at TIMESTAMP NULL,
     created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP NULL
+    updated_at TIMESTAMP NULL,
+    deleted_at TIMESTAMP NULL
 );
 
 CREATE TABLE certificates
@@ -134,9 +146,10 @@ CREATE TABLE certificates
     program_id BIGINT REFERENCES programs (id) NOT NULL,
     user_id BIGINT REFERENCES users (id) NOT NULL,
     url VARCHAR(256) NOT NULL,
-    graduated_at TIMESTAMP NOT NULL,
+    issued_at TIMESTAMP NOT NULL,
     created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP NULL
+    updated_at TIMESTAMP NULL,
+    deleted_at TIMESTAMP NULL
 );
 
 CREATE TABLE discussions
@@ -144,8 +157,10 @@ CREATE TABLE discussions
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     text VARCHAR(2048) NOT NULL,
     lesson_id BIGINT REFERENCES lessons (id) NOT NULL,
+    user_id BIGINT REFERENCES users (id) NOT NULL,
     created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP NULL
+    updated_at TIMESTAMP NULL,
+    deleted_at TIMESTAMP NULL
 );
 
 CREATE TYPE article_status AS ENUM ('created', 'in moderation', 'published', 'archived');
@@ -153,9 +168,10 @@ CREATE TABLE blogs
 (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     user_id BIGINT REFERENCES users (id) NOT NULL,
-    title VARCHAR(256) NOT NULL,
-    text VARCHAR(8192) NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    content VARCHAR(8192) NOT NULL,
     status article_status NOT NULL,
     created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP NULL
+    updated_at TIMESTAMP NULL,
+    deleted_at TIMESTAMP NULL
 );
